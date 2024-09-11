5f30f2892aaa5e3a5fce99a54b5dd7e45e75ac18
unknown <khushboosongara47@gmail.com>
2024-09-11 12:28:30 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrls: ['./app.component.less'],
+  imports: [HeaderComponent, BannerComponent, ProductListComponent, FooterComponent] // Import other standalone components here
 })
 export class AppComponent {
   title = 'flipkart-clone';
